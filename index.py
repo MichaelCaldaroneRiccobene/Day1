61eac3bac730f8b7c89eb34728d855389ff544a3
Mike <riccobene.m.c002@gmail.com>
2025-11-04 19:41:14 +0100

    Day 1

---
+++ index.py
@@ -0,0 +1,5 @@
+print("Hello, " + input("Your Name") + " today we create a newName! ")
+nameFantastic = input("Type a Fantastic Word").capitalize()
+nameFantasticObj = input("Type a Obj").capitalize()
+
+print("The fanstatic name is, " + nameFantastic + nameFantasticObj + "!" )
